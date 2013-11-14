09d945bf509e7aa357a94d539b6999f52d119a8a
Alex Ashley <alex@ashley-family.net>
2013-11-14 14:18:06 +0000

    added detection of apt-get or yum

--- generic-install.sh
+++ generic-install.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
-sudo apt-get -y install xboxdrv
-sudo apt-get -y install lirc
-sudo apt-get -y install python-dev
-sudo apt-get -y install python-pip
+APTGET=`which apt-get`
+if [ -z "${APTGET}" ] ; then
+    APTGET=`which yum`
+fi
+if [ -z "${APTGET}" ] ; then
+    echo "FATAL ERROR: Failed to find apt-get or yum package installers"
+    exit 1
+fi
+sudo ${APTGET} -y install xboxdrv
+sudo ${APTGET} -y install lirc
+sudo ${APTGET} -y install python-dev
+sudo ${APTGET} -y install python-pip
 sudo cp lircd.conf /etc/lirc/
 sudo cp hardware.conf /etc/lirc/
 sudo /etc/init.d/lirc restart
 sudo cp xbox-daemon /usr/local/sbin
-sudo chmod u+rx /usr/local/sbin/xbox-daemon
+sudo chmod a+rx /usr/local/sbin/xbox-daemon
 sudo python setup.py install
 sudo cp 80-xbox360.rules /etc/udev/rules.d
 sudo chmod u+rx /etc/udev/rules.d/80-xbox360.rules
